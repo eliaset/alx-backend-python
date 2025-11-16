e12934111491d310b7a797585e00a2de2d43ccd5
eliaset <eliasshamil@gmail.com>
2025-11-16 19:08:05 +0300

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.access_nested_map."""
+"""Unit tests for utils.access_nested_map exceptions."""
 
 import unittest
 from parameterized import parameterized
@@ -7,13 +7,14 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test access_nested_map function."""
+    """Test access_nested_map exceptions."""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Test that access_nested_map returns correct value."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map_exception(self, nested_map, path, key):
+        """Test that KeyError is raised for missing keys."""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), f"'{key}'")
