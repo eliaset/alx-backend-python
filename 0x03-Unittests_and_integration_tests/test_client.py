75bbaa3aab2f1fa3064a5366ef79958b20222f83
eliaset <eliasshamil@gmail.com>
2025-11-16 22:21:27 +0300

    update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-"""Unit tests for GithubOrgClient - Tasks 4, 5, 6, 7"""
-
 import unittest
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
